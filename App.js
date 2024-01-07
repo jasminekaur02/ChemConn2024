0ada6b081e6d0ad30ae17740ae2c5efe4baa354c
Jasmine Kaur <jasminekaurmukkar@gmail.com>
2024-01-08 01:37:35 +0530

    Chemconf2024 made

---
+++ App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import CustomNavbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import './Global.css'; // Import the global styles
+
+const App = () => {
+  return (
+    <div>
+      <CustomNavbar />
+      <HeroSection />
+      {/* Add other components or sections as needed */}
+    </div>
+  );
+};
+
+export default App;
